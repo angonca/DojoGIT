68e0616c5998070d92a116fd28de388ad28b559d
Angel Gonzalez <angonca74@gmail.com>
2026-04-01 23:41:53 +0200

    Commit to reverse

--- User.cs
+++ User.cs
@@ -2,7 +2,5 @@ public class User
 {
     public int Id { get; set; }
     public string Fistname { get; set; }
-    public string Lastname { get; set; }
-    public string Email { get; set; }
     public string Password { get; set; }
 }
